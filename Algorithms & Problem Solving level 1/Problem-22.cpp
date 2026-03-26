9cf71646614ee6785e7935da54ab1d651c5bd803
Islam Raad <islamraad.dev@gmail.com>
2026-03-26 18:31:18 +0300

    Update Problem-22.cpp

--- Algorithms & Problem Solving level 1/Problem-22.cpp
+++ Algorithms & Problem Solving level 1/Problem-22.cpp
@@ -1,6 +1,6 @@
 #include <iostream>   // Include iostream for input/output operations.
 #include <string>     // Include string for potential future use.
-#include <cmath>      // Include cmath to use pow() for exponentiation.
+#include <cmath>      // Include cmath to use pow() for exponentiation. 
  
 using namespace std;  // Use the standard namespace to avoid prefixing std:: to standard functions.
 
